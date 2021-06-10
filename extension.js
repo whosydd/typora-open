d0d3d6491a9fb3f9fda253f1602ea6a42b559866
whosydd <dev.youngkwok718@gmail.com>
2021-06-11 01:55:52 +0800

    去掉冗余代码

--- extension.js
+++ extension.js
@@ -1,8 +1,6 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode')
-const fs = require('fs')
-
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 
